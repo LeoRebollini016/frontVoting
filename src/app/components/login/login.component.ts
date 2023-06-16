212db1901f1b74683b43661ac7bbfe1499a9bd95
leo <LeoRebollini@gmail.com>
2023-06-16 16:42:31 -0300

    components, login and css

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit{
+  public state: boolean;
+  
+  constructor(){
+    this.state = true;
+  }
+  ngOnInit(): void {
+    $('p a').click(function(){
+      $('form').animate({height: "toggle", opacity:"toggle"}, "slow");
+    }); 
+    if(this.state)
+      $('.reg-formlogin-form"').animate({display: "none"});
+    else
+      $('.login-form').animate({display:"none"});
+  }
+  setStateLogin(state: boolean){
+      this.state = state;
+  }
 }
